29b75cd73c9d66606bdd46b73a1ee929dacd7446
Tmk1988 <tymekf@gmail.com>
2017-05-04 18:17:39 +0200

    initial

---
+++ src/main/java/org/tymfry/CarDealerSystemApplication.java
@@ -0,0 +1,12 @@
+package org.tymfry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealerSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/org/tymfry/CarDealerSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tymfry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarDealerSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
